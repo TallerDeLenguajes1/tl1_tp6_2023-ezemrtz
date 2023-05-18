59faeeeccf3482dab7f5450acb1b7a79983657ab
Ezequiel Martinez <ezequielmartinez2424@gmail.com>
2023-05-18 18:58:09 -0300

    Ejercicio 1 realizado

---
+++ practico6/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num = 0;
+string? numString = "", numInvS = "";
+bool anda = false;
+
+while(!anda){
+    Console.WriteLine("Ingrese un numero:");
+    numString = Console.ReadLine();
+    anda = int.TryParse(numString, out num);
+    if(!anda){
+        Console.WriteLine(numString + " no es un numero valido.");
+    }
+}
+if(num > 0){
+    int aux;
+    while (num != 0){
+        aux = num % 10;
+        num /= 10;
+        numInvS = numInvS + aux;
+    }
+    Console.WriteLine(numInvS);
+}
+
+
