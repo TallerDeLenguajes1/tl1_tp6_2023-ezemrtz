3fa1f756932d466fbf8ae1ac859c51c556d7adfe
“Ezequiel <ezequielmartinez2424@gmail.com>
2023-05-25 20:30:39 -0300

    Avance ej4

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,27 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-//Ejercicio 1
-
-int num = 0;
-string? numString = "", numInvS = "";
+int opcion = 0;
 bool anda = false;
-
+Console.WriteLine("Que ejercicio quiere realizar: ");
+Console.WriteLine("1- Ejercicio 1 ");
+Console.WriteLine("2- Ejercicio 4");
 while(!anda){
     Console.WriteLine("Ingrese un numero:");
-    numString = Console.ReadLine();
-    anda = int.TryParse(numString, out num);
+    anda = int.TryParse(Console.ReadLine(), out opcion);
     if(!anda){
-        Console.WriteLine(numString + " no es un numero valido.");
+        Console.WriteLine("No es un numero valido.");
     }
 }
-if(num > 0){
-    int aux;
-    while (num != 0){
-        aux = num % 10;
-        num /= 10;
-        numInvS = numInvS + aux;
-    }
-    Console.WriteLine(numInvS);
-}
+switch (opcion)
+{
+    case 1:
+        //Ejercicio 1
+        int num = 0;
+        string? numString = "", numInvS = "";
+        anda = false;
+        while(!anda){
+            Console.WriteLine("Ingrese un numero:");
+            numString = Console.ReadLine();
+            anda = int.TryParse(numString, out num);
+            if(!anda){
+                Console.WriteLine(numString + " no es un numero valido.");
+            }
+        }
+        if(num > 0){
+            int aux;
+            while (num != 0){
+                aux = num % 10;
+                num /= 10;
+                numInvS = numInvS + aux;
+            }
+            Console.WriteLine(numInvS);
+        }
+    break;
 
-// Ejercicio 4
+    case 2:
+        // Ejercicio 4
+        string? cadena1, cadena2;
+        Console.WriteLine("Ingrese un cadena de caracteres: ");
+        cadena1 = Console.ReadLine();
+
+        Console.WriteLine("La longitud de la cadena es: "+ cadena1.Length);
+        Console.WriteLine("Ingrese una segunda cadena de caracteres: ");
+        cadena2 = Console.ReadLine();
+
+        Console.WriteLine("La cadena concatenada es: "+ cadena1 + cadena2);
+    break;
+
+    default:
+    break;
+}
